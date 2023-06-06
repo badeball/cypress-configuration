9c28e192f244911d65dd2f9fce47881089672bf6
Jonas Amundsen <jonasba@gmail.com>
2023-06-06 10:53:31 +0200

    Reduce some repetition
    In preparation of adding another value.

--- lib/cypress-configuration/cypress-post10-configuration-parser.test.ts
+++ lib/cypress-configuration/cypress-post10-configuration-parser.test.ts
@@ -1,102 +1,115 @@
+import { inspect } from "util";
+
 import assert from "assert/strict";
 
-import { ICypressPost10Configuration } from "./cypress-post10-configuration";
+import {
+  ICypressPost10Configuration,
+  TestingType,
+} from "./cypress-post10-configuration";
 
 import {
   ConfigurationFile,
+  TestConfiguration,
   parsePost10Configuration,
 } from "./cypress-post10-configuration-parser";
 
-let examples = 1;
+function createSourceCode<T extends keyof TestConfiguration>(options: {
+  type: "cjs" | "esm";
+  withDefineCall: boolean;
+  testingType: TestingType;
+  property: T;
+  value: TestConfiguration[T];
+}) {
+  const {
+    type,
+    withDefineCall,
+    testingType,
+    property,
+    value: rawValue,
+  } = options;
 
-function example(source: string, expected: ConfigurationFile) {
-  it(`example #${examples++}`, () => {
-    assert.deepStrictEqual(parsePost10Configuration(source), expected);
-  });
+  const value = inspect(rawValue);
+
+  if (type === "cjs") {
+    if (withDefineCall) {
+      return `module.exports = defineConfig({ ${testingType}: { ${property}: ${value} } });`;
+    } else {
+      return `module.exports = { ${testingType}: { ${property}: ${value} } };`;
+    }
+  } else {
+    if (withDefineCall) {
+      return `export default defineConfig({ ${testingType}: { ${property}: ${value} } });`;
+    } else {
+      return `export default { ${testingType}: { ${property}: ${value} } };`;
+    }
+  }
+}
+
+function example<T extends keyof TestConfiguration>(options: {
+  testingType: TestingType;
+  property: T;
+  value: TestConfiguration[T];
+}) {
+  const { testingType, property, value } = options;
+
+  const expected: ConfigurationFile = {
+    [testingType]: {
+      [property]: value,
+    },
+  };
+
+  for (const type of ["cjs", "esm"] as const) {
+    for (const withDefineCall of [false, true]) {
+      const description = `${type}, ${
+        withDefineCall ? "with" : "without"
+      } defineConfig`;
+
+      it(description, () => {
+        assert.deepStrictEqual(
+          parsePost10Configuration(
+            createSourceCode({
+              type,
+              withDefineCall,
+              testingType,
+              property,
+              value,
+            })
+          ),
+          expected
+        );
+      });
+    }
+  }
 }
 
 describe("parsePost10Configuration()", () => {
-  for (const testingType of ["e2e", "component"]) {
+  for (const testingType of ["e2e", "component"] as const) {
     describe(testingType, () => {
       for (const property of ["specPattern", "excludeSpecPattern"] as const) {
-        example(
-          `module.exports = { ${testingType}: { ${property}: 'foo/bar' } };`,
-          {
-            [testingType]: { [property]: "foo/bar" },
-          }
-        );
+        describe(`${property} with string`, () => {
+          example({
+            testingType,
+            property,
+            value: "foo/bar",
+          });
+        });
 
-        example(
-          `module.exports = { ${testingType}: { ${property}: ['foo/bar'] } };`,
-          {
-            [testingType]: { [property]: ["foo/bar"] },
-          }
-        );
-
-        example(
-          `module.exports = defineConfig({ ${testingType}: { ${property}: 'foo/bar' } });`,
-          {
-            [testingType]: { [property]: "foo/bar" },
-          }
-        );
-
-        example(
-          `module.exports = defineConfig({ ${testingType}: { ${property}: ['foo/bar'] } });`,
-          {
-            [testingType]: { [property]: ["foo/bar"] },
-          }
-        );
-
-        example(
-          `export default { ${testingType}: { ${property}: 'foo/bar' } };`,
-          {
-            [testingType]: { [property]: "foo/bar" },
-          }
-        );
-
-        example(
-          `export default { ${testingType}: { ${property}: ['foo/bar'] } };`,
-          {
-            [testingType]: { [property]: ["foo/bar"] },
-          }
-        );
-
-        example(
-          `export default defineConfig({ ${testingType}: { ${property}: 'foo/bar' } });`,
-          {
-            [testingType]: { [property]: "foo/bar" },
-          }
-        );
-
-        example(
-          `export default defineConfig({ ${testingType}: { ${property}: ['foo/bar'] } });`,
-          {
-            [testingType]: { [property]: ["foo/bar"] },
-          }
-        );
+        describe(`${property} with string-array`, () => {
+          example({
+            testingType,
+            property,
+            value: ["foo/bar"],
+          });
+        });
       }
 
-      example(`module.exports = { ${testingType}: { env: { foo: 'bar' } } };`, {
-        [testingType]: { env: { foo: "bar" } },
+      describe("env", () => {
+        example({
+          testingType,
+          property: "env",
+          value: { foo: "bar" },
+        });
       });
-
-      example(
-        `module.exports = defineConfig({ ${testingType}: { env: { foo: 'bar' } } });`,
-        {
-          [testingType]: { env: { foo: "bar" } },
-        }
-      );
-
-      example(`export default { ${testingType}: { env: { foo: 'bar' } } };`, {
-        [testingType]: { env: { foo: "bar" } },
-      });
-
-      example(
-        `export default defineConfig({ ${testingType}: { env: { foo: 'bar' } } });`,
-        {
-          [testingType]: { env: { foo: "bar" } },
-        }
-      );
     });
   }
 });
